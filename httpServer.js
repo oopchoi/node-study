97c7be9d703e4d0642d0e9c70bc53531e13e91fb
oopchoi <oopchoi@gmail.com>
2018-03-20 00:15:50 +0900

    code 3-4 HTTP 서버 전체 코드

---
+++ httpServer.js
@@ -0,0 +1,7 @@
+const http = require('http');   // http 모듈 로드
+
+var server = http.createServer((req, res) => {  // createServer 함수를 이용해 인스턴스 생성
+    res.end("hello world");     // hello world 응답
+});
+
+server.listen(8000);    // 8000번 포트로 리슨
